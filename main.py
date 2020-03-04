49fd9c3d7e2fdf930f6e85c5776747a395e3879e
PaulaMihalcea <mihalceapaula@gmail.com>
2020-03-04 20:08:02 +0100

    Fixed variable assignment in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from plot_inkml import plot_inkml
 n = 14
 
 for i in range(0, n+1):
-    svg_file = inkml_file = './examples/example' + str(i) + '.svg'  # svg file to be converted to inkml
+    svg_file = './examples/example' + str(i) + '.svg'  # svg file to be converted to inkml
     inkml_file = './examples/example' + str(i) + '.inkml'  # Converted file name
     svg2inkml(svg_file, inkml_file)  # Converts svg file to inkml
     plot_inkml(inkml_file)  # Plots the returned data
